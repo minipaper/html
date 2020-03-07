dde59cc7f0f2c1fdb9d6c792892f3fbc2650de3d
minipaper <pianoday@gmail.com>
2020-03-07 16:03:37 +0900

    update

---
+++ _nuxt/61d10dccc6ef3074f862.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{149:function(t,e,n){"use strict";n.r(e);var l={},r=n(22),component=Object(r.a)(l,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("Hi html")])])}],!1,null,null,null);e.default=component.exports}}]);
